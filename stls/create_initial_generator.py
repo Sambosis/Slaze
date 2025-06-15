425bb14f861a1399cc4bb8c3b9345c663a8efc01
Sambosis <sambosis@gmail.com>
2025-06-14 20:31:15 -0400

    Update generator initialization to use CodeLlama-7b-hf model

--- stls/create_initial_generator.py
+++ stls/create_initial_generator.py
@@ -1,7 +1,8 @@
 from lspo_3d.models.generator import CadQueryGenerator
 
 # Initialize a generator with a standard pre-trained model
-generator = CadQueryGenerator(model_name_or_path="distilgpt2")
+# generator = CadQueryGenerator(model_name_or_path="distilgpt2")
+generator = CadQueryGenerator(model_name_or_path="codellama/CodeLlama-7b-hf")
 
 # Save the model to a directory
 generator.save_model("./stls/lspo_3d/initial_generator")
